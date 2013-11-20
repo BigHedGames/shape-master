917a6ff609df3115d14c1e19d9f0cf6b43cbb574
ducksFANjason <jasonurbaniec@gmail.com>
2013-11-19 20:47:18 -0800

    Committed Enums.cs (because I mistakenly left it off before)

---
+++ ShapeMaster/ShapeMaster/ShapeMaster/Enums.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShapeMaster
+{
+    // declare enumeration that stores the player shape status
+    public enum ShapeStatus { Circle, Square, Star, Triangle };
+}
